95d2c41925c0d00c63096da113369093224e0247
julesyg <111521163+julesyg@users.noreply.github.com>
2022-10-08 17:46:03 -0400

    queries

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE employees (emp_no INT NOT NULL,
      PRIMARY KEY (emp_no)
 );
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -32,9 +32,9 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 CREATE TABLE emp_no (
+dept_no VARCHAR(4) NOT NULL,
 emp_no INT NOT NULL,
-dept_no INT NOT NULL,
-from_date DATE NOT NULL
+from_date DATE NOT NULL,
 to_date DATE NOT NULL,
 PRIMARY KEY (emp_no, dept_no)
 );
@@ -42,6 +42,7 @@ PRIMARY KEY (emp_no, dept_no)
 CREATE TABLE titles (
 emp_no INT NOT NULL,
 from_date DATE NOT NULL, 
-to_date DATE NOT NULL
+to_date DATE NOT NULL,
+-- FOREIGN KEY (emp_no) REFERENCES (emp_no),
 PRIMARY KEY (emp_no)
 );
